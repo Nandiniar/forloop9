6a845b10959bea494c2502d34300f3e702c81ee8
nandini <nandani@contaque.com>
2025-05-07 15:01:11 +0530

    Add the changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forloop9';
+  users=['X','Y','Z','A','B'];
+  students=[
+    {
+      name:'X',
+      age:19,
+      email:'X@gmail.com'
+    },
+    {
+      name:'Y',
+      age:21,
+      email:'Y@gmail.com',
+
+    },
+    {
+      name:'Z',
+      age:22,
+      email:'Z@gmail.com'
+    },
+    {
+      name:'A',
+      age:28,
+      email:'A@gmail.com'
+    }
+  ];
+  getName(val:string){
+    console.log(val);
+  }
 }
